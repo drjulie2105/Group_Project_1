a4cd4c4831f5d0ca4f2ef6a409f3b5b936fc207d
Julie Pyle <drjuliepyle@gmail.com>
2020-09-10 19:34:41 -0400

    Added headers

--- Crime_Cuyahoga/api_keys.py
+++ Crime_Cuyahoga/api_keys.py
@@ -4,6 +4,9 @@ weather_api_key = "7203eacdc5ea51293732d8440f1cdf92"
 # FBI API Key ??
 fbi_key = "PMG93AJsfl2OfmOfaA4C6lNUZpAwzmSoSfhsDPDT"
 
+#census API
+census_key = "80fc94c33b7171a8eec9a3b40f85462d3741b0eb"
+
 
 
 
