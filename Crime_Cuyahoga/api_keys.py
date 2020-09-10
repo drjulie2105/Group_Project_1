8dd122e78185e7385becdf6b449463007896b2e8
Kristen <68074437+KristenBlanchard@users.noreply.github.com>
2020-09-10 18:44:37 -0400

    Heat Maps

--- Crime_Cuyahoga/api_keys.py
+++ Crime_Cuyahoga/api_keys.py
@@ -4,6 +4,8 @@ weather_api_key = "7203eacdc5ea51293732d8440f1cdf92"
 # FBI API Key ??
 fbi_key = "PMG93AJsfl2OfmOfaA4C6lNUZpAwzmSoSfhsDPDT"
 
+# gmaps API key
+g_key = "AIzaSyBJKuQ833RemYUvLu4-hZ_0e-hw_17Sqcw"
 
 
 
