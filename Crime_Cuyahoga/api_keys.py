7a44a7ad04413d6cf6d2035c57443521490edc6c
Julie Pyle <drjuliepyle@gmail.com>
2020-09-12 12:00:30 -0400

    Added updated file

--- Crime_Cuyahoga/api_keys.py
+++ Crime_Cuyahoga/api_keys.py
@@ -7,6 +7,8 @@ fbi_key = "PMG93AJsfl2OfmOfaA4C6lNUZpAwzmSoSfhsDPDT"
 #census API
 census_key = "80fc94c33b7171a8eec9a3b40f85462d3741b0eb"
 
+g_key = "AIzaSyBW75poaZg1UMp6D2ldq9utOJxdri8tSbs"
+
 
 
 
